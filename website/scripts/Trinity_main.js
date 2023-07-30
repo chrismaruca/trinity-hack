8d08d3e50ae4c74ad84f200c062c63727bc4869a
chrismaruca <chrismaruca22@gmail.com>
2023-07-29 19:10:17 -0700

    add slideshows to website

--- website/scripts/Trinity_main.js
+++ website/scripts/Trinity_main.js
@@ -45,3 +45,39 @@ const observer = new MutationObserver((mutations) => {
 })
 
 observer.observe(gas, {attributes: true, characterData: true, subtree: true, childList: true});
+
+// Slideshow code
+let curr_slide = [0, 0];
+let slide_arr = ["slides0", "slides1"];
+
+// Show the first slide of every slideshow
+for (let s = 0; s < slide_arr.length; s++) {
+    chooseSlide(0, s);
+}
+
+function switchSlide(d, n) {
+    renderSlides(curr_slide[n] + d, n);
+}
+
+function chooseSlide(index, n) {
+    renderSlides(index, n);
+}
+
+function renderSlides(index, n) {
+    let slides = document.getElementsByClassName(slide_arr[n]);
+
+    curr_slide[n] = index;
+
+    if (curr_slide[n] === slides.length) {
+        curr_slide[n] = 0;
+    } else if (curr_slide[n] < 0) {
+        curr_slide[n] = slides.length - 1;
+    }
+
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[curr_slide[n]].style.display = "block";
+}
